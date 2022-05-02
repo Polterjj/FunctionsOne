6bd031c0df2eb64187a3e53225dbd2fde13c919b
JJ <jjpdet@gmail.com>
2022-05-02 18:56:35 -0400

    Added Function.js file and Index.html file

---
+++ functions.js
@@ -0,0 +1,25 @@
+function getAreaOfCircle (radius){
+    return MathPi * radius * radius
+}
+
+console.log (getAreaOfCircle (1));
+
+
+
+function getCircOfCircle (radius){
+    return  2 * 3.14 * radius;
+}
+
+console.log (getCircOfCircle (2));
+
+function getAreaOfSquare (side){
+
+    return 
+}
+
+function getAreaOfTriangle (base, height){
+    return 1/2 * base * height;
+}
+console.log (getAreaOfTriangle (2,3));
+
+
